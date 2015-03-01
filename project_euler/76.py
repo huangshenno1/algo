2a23bbe3e369724aafd7772ba0898b2cbd2a1e22
huangshenno1 <huangshenno1@gmail.com>
2015-03-01 10:56:40 +0800

    Added solution for problem 76.(Pentagonal number theorem)

---
+++ project_euler/76.py
@@ -0,0 +1,17 @@
+a1 = lambda k: (-(-1)**k, k*(3*k-1)/2)
+a2 = lambda k: (-(-1)**k, k*(3*k+1)/2)
+
+maxn = 100
+p = [1] + [0] * maxn
+for n in xrange(1, maxn+1):
+	i = 1
+	while True:
+		(f, k) = a1(i)
+		if k <= n: p[n] += f*p[n-k]
+		else: break
+		(f, k) = a2(i)
+		if k <= n: p[n] += f*p[n-k]
+		else: break
+		i += 1
+
+print p[100]-1
